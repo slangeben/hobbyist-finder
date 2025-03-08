96e300f229689d65bc35282ae45d8c446e080476
Ching Yee Huang <huangchingyee@gmail.com>
2025-03-08 16:28:31 +0000

    Update FindInterest.jsx

--- src/FindInterest.jsx
+++ src/FindInterest.jsx
@@ -68,7 +68,7 @@ const FindInterest = () => {
                         <p className="card-text">{event.name}</p>
                         <div className="d-flex justify-content-between align-items-center">
                             <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleTech} >View</button>
                             <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
                         </div>
